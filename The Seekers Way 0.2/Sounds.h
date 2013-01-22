24f9db3398c3d2e298fa95837b0e1e9d6c79a9c7
Peter <peter.nilsson.1991@hotmail.com>
2013-01-22 17:57:01 +0100

    Sound
    h och cpp skapade och v.1 klart.

---
+++ The Seekers Way 0.2/Sounds.h
@@ -0,0 +1,35 @@
+#ifndef INCLUDED_SOUND
+#define INCLUDED_SOUND
+#include <SFML/Audio.hpp>
+#include <vector>
+#include <string>
+#include <map>
+
+class Sounds
+{
+public:
+
+	~Sounds();
+	static Sounds* getInstance();
+	void Play(std::string namn);
+	void Play(std::string namn, float volume);
+	void Loop(std::string namn);
+	void Loop(std::string namn, float volume);
+	void StopAll();
+	void PauseAll();
+	void UnPauseAll();
+	void setMasterVolume(float volume);
+	float getMasterVolume();
+
+private:
+	
+	Sounds();
+	static Sounds *sInstance;
+	typedef std::vector<sf::Sound*> SoundVector;
+	SoundVector mSounds;
+	typedef std::map<std::string, sf::SoundBuffer*> BufferMap;
+	BufferMap bufferlista;
+	void Playbase(std::string namn, float volume, bool repeat);
+};
+
+#endif
