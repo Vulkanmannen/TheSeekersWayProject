a3af893312eb16b88cb62801d8677f83d8e27815
Christofer Fransson <Choffop@gmail.com>
2013-01-31 14:16:22 +0100

    shield class

---
+++ The Seekers Way 0.2/Shield.cpp
@@ -0,0 +1,24 @@
+#include "Shield.h"
+#include "ImageManager.h"
+
+Shield::Shield(sf::Vector2f &position)
+	mAnimation("shield.png", 60, 1, HEIGHT, WIDTH)
+{
+
+}
+
+
+Shield::~Shield()
+{
+}
+
+void update()
+{
+
+}
+
+void render()
+{
+	mAnimation.setPosition(sf::Vector2f(mPosition.x - WIDTH/ 2, mPosition.y - HEIGHT/ 2));
+	ImageManager::render(&mAnimation.getSprite());
+}
