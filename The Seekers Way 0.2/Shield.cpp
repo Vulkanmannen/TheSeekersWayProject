65bd464260231ae8b3347eefba46aae5531edddf
Peter <peter.nilsson.1991@hotmail.com>
2013-01-31 15:50:38 +0100

    asd

--- The Seekers Way 0.2/Shield.cpp
+++ The Seekers Way 0.2/Shield.cpp
@@ -1,7 +1,6 @@
 #include "Shield.h"
 #include "ImageManager.h"
 
-<<<<<<< HEAD
 
 static const float WIDTH = 64;
 static const float HEIGHT = 128;
@@ -31,7 +30,6 @@ void Shield::render()
 	mAnimation.setPosition(sf::Vector2f(mPosition.x - WIDTH/ 2, mPosition.y - HEIGHT/ 2));
 	ImageManager::render(&mAnimation.getSprite());
 }
-=======
 //Shield::Shield(sf::Vector2f &position)
 //	mAnimation("shield.png", 60, 1, HEIGHT, WIDTH)
 //{
@@ -53,4 +51,3 @@ void Shield::render()
 //	mAnimation.setPosition(sf::Vector2f(mPosition.x - WIDTH/ 2, mPosition.y - HEIGHT/ 2));
 //	ImageManager::render(&mAnimation.getSprite());
 //}
->>>>>>> 1b171f5f25b860cdad9eaac767258f9fe1f8b960
