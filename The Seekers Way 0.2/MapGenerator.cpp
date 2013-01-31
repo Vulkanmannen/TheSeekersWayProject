10a6828619114ee9ad467ffee7853b75fb3d93a7
Alexander Karlsson <alexander90karlsson@hotmail.com>
2013-01-31 16:55:28 +0100

    sdf

--- The Seekers Way 0.2/MapGenerator.cpp
+++ The Seekers Way 0.2/MapGenerator.cpp
@@ -29,29 +29,41 @@ void MapGenerator::generateMap(std::string imageName)
 			sf::Color color = image.getPixel(i ,j);
 			sf::Vector2f position = sf::Vector2f(i * 64.f, j *64.f);
 
+			EntityManager* entityManager = EntityManager::getInstance();
+
 			if(color.r == 0 && color.a == 255)
 			{
-				EntityManager::getInstance()->addEntity(new NormalBlock(position, color.b));
+				entityManager->addEntity(new NormalBlock(position, color.b));
 			}
 
 			else if(color.r == 100 && color.a == 255)
 			{	
+				Entity* entity; 
+				
 				if(color.g == 0)
-				{
-					EntityManager::getInstance()->addCharacter(new Fenrir(position), 1);
+				{	
+					entity = new Sheeka(position);
+					entityManager->addCharacter(static_cast<Character*>(entity), 0);
 				}
 				else if(color.g == 100)
 				{
-					EntityManager::getInstance()->addCharacter(new Sheeka(position), 0);
+					entity = new Fenrir(position);
+					entityManager->addCharacter(static_cast<Character*>(entity), 1);
 				}
 				else if(color.g == 200)
 				{
-					EntityManager::getInstance()->addCharacter(new Charlotte(position), 2);
+					entity = new Charlotte(position);
+					entityManager->addCharacter(static_cast<Character*>(entity), 2);
 				}
 				else if(color.g == 255)
 				{
-					EntityManager::getInstance()->addCharacter(new Kiba(position), 3);
+					entity = new Kiba(position);
+					entityManager->addCharacter(static_cast<Character*>(entity), 3);
 				}
+				
+				entityManager->addCharacter(static_cast<Character*>(entity), 1);
+				entityManager->addEntity(entity);
+				entityManager->addDynamicEntity(entity);
 			}
 		}
 	}
