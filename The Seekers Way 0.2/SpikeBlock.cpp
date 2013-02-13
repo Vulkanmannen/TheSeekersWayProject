0ec0ec0015aa28597182d34dde39c46b1deff48d
Alexander Karlsson <alexander90karlsson@hotmail.com>
2013-02-13 13:09:51 +0100

    fix

--- The Seekers Way 0.2/SpikeBlock.cpp
+++ The Seekers Way 0.2/SpikeBlock.cpp
@@ -7,7 +7,7 @@ static const float HEIGHT = 64;
 static const float WIDTH = 64;
 
 SpikeBlock::SpikeBlock(sf::Vector2f &position):
-	mAnimation("Spikeblock.png", 60, 1, HEIGHT, WIDTH)
+	mAnimation("spikerune.png", 60, 12, HEIGHT, WIDTH)
 {
 	mPosition = position;
 	mWidth = WIDTH;
@@ -24,12 +24,12 @@ SpikeBlock::~SpikeBlock()
 
 void SpikeBlock::update(EntityKind &currentEntity)
 {
-	
+	mAnimation.update(0);
 }
 
 void SpikeBlock::render()
 {
-	mAnimation.setPosition(sf::Vector2f(mPosition.x - WIDTH/ 2, mPosition.y - HEIGHT/ 2));
+	//mAnimation.setPosition(sf::Vector2f(mPosition.x - WIDTH/ 2, mPosition.y - HEIGHT/ 2));
 	ImageManager::render(&mAnimation.getSprite());
 }
 
