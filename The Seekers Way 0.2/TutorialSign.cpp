3e5e1b18b43f7e1208ac5557431aa69e82685da7
Christofer Fransson <Choffop@gmail.com>
2013-03-13 18:56:19 +0100

    Signs one step futher

--- The Seekers Way 0.2/TutorialSign.cpp
+++ The Seekers Way 0.2/TutorialSign.cpp
@@ -1,90 +1,57 @@
 #include "TutorialSign.h"
 #include "ImageManager.h"
 #include "Entity.h"
+#include "ZoomedSign.h"
+#include "EntityManager.h"
 
-static const float WIDTH	= 64;
+
+static const float WIDTH	= 40;
 static const float HEIGHT	= 64;
 
-static const float ZOOMEDWIDTH	= 800;
-static const float ZOOMEDHEIGHT	= 563;
 
-
-TutorialSign::TutorialSign(sf::Vector2f &position):
-	kibaOnSign(false),
-	charlotteOnSign(false),
-	fenrirOnSign(false),
-	sheekaOnSign(false)
+TutorialSign::TutorialSign(sf::Vector2f &position)
 {
 	mPosition = position + sf::Vector2f(64, 64);
 	mAlive = true;
 	mWidth = WIDTH;
 	mHeight = HEIGHT;
-	mEntityKind = TUTORIALSIGN;
+	mEntityKind = TUTORIALSIGN;	
 	mLayer = MIDDLE;
+
+	mZoomedSign = new ZoomedSign(mPosition);
+	EntityManager::getInstance()->addEntity(mZoomedSign);
 
 	mTexture.loadFromImage(*ImageManager::getImage("TutorialSign.PNG"));
 	mSprite.setTexture(mTexture);
 	mSprite.setPosition(mPosition);
 	mSprite.setOrigin(mSprite.getLocalBounds().width / 2, mSprite.getLocalBounds().height / 2);
-
-	mZoomedSignTexture.loadFromImage(*ImageManager::getImage("ZoomedTutorialSign.png"));
-	mZoomedSignSprite.setTexture(mZoomedSignTexture);
-	mZoomedSignSprite.setPosition(mPosition + sf::Vector2f(0, -194));
-	mZoomedSignSprite.setOrigin(mZoomedSignSprite.getLocalBounds().width / 2, mZoomedSignSprite.getLocalBounds().height / 2);
 }
-
 
 TutorialSign::~TutorialSign()
 {
+	
 }
 
 void TutorialSign::update(EntityKind &currentEntity)
 {
+	mCurrent = currentEntity;
 	onSignFalse();
 }
 
 void TutorialSign::render()
 {
-	if(kibaOnSign == true)
-	{
-		ImageManager::render(&mZoomedSignSprite);
-	}
-	if(charlotteOnSign == true)
-	{
-		ImageManager::render(&mZoomedSignSprite);
-	}
-
 	ImageManager::render(&mSprite);
 }
 
 void TutorialSign::interact(Entity* e)
 {
-	if((*e) == KIBA)
+	if((*e) == CHARACTER && e->getEntityKind() == mCurrent)
 	{
-		kibaOnSign = true;
-	}
-	else if((*e) == CHARLOTTE)
-	{
-		charlotteOnSign = true;
-	}
-	else if((*e) == FENRIR)
-	{
-		fenrirOnSign = true;
-	}
-	else if((*e) == SHEEKA)
-	{
-		sheekaOnSign = true;
+		mZoomedSign->setCharacterOnSign(true);
 	}
 }
 
 void TutorialSign::onSignFalse()
 {
-	if(mClock.getElapsedTime().asMilliseconds() >= 100)
-	{
-		kibaOnSign		= false;
-		charlotteOnSign	= false;
-		fenrirOnSign	= false;
-		sheekaOnSign	= false;
-		mClock.restart();
-	}
+		mZoomedSign->setCharacterOnSign(false);
 }
